e809aa548d4525d4ba36f97fc60d60b7e1d691c2
lizziestenhaug <estenhaug@gmail.com>
2021-03-08 21:09:24 -0600

    target

--- SBUX_app.py
+++ SBUX_app.py
@@ -1,206 +1,257 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# Dependencies
-import math
-import pandas_datareader as web
-from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-plt.style.use('fivethirtyeight')
-
-
-# In[2]:
-
-
-# get the stock quote
-SBUX_df = web.DataReader('SBUX', data_source='yahoo', start='2000-01-01', end ='2021-12-31')
-SBUX_df
-
-
-# In[3]:
-
-
-# get the total rows and columns in the AAPL Dataset
-SBUX_df.shape
-
-
-# In[5]:
-
-
-# create a closing price dataset
-Closing_Price_SBUX_df = SBUX_df.filter(['Close'])
-
-# convert the the closing price dataset to a array
-Closing_Price_array_SBUX_df = Closing_Price_SBUX_df.values
-
-Closing_Price_array_SBUX_df
-
-
-# In[6]:
-
-
-# get the number of rows to train the model on (90 %)
-train_closing_price_SBUX_len = math.ceil(len(Closing_Price_array_SBUX_df) * 0.9) # rounded up
-
-train_closing_price_SBUX_len
-
-
-# In[7]:
-
-
-# Scale de closing price dataset between 0 and 1
-scale_SBUX = MinMaxScaler(feature_range=(0,1))
-scaled_closing_price_SBUX_df = scale_SBUX.fit_transform(Closing_Price_array_SBUX_df)
-scaled_closing_price_SBUX_df
-
-
-# In[8]:
-
-
-# Training Data
-train_closing_price_SBUX = scaled_closing_price_SBUX_df[0:train_closing_price_SBUX_len, :]
-# Split Data
-x_train =[]
-y_train=[]
-
-for i in range(80, len(train_closing_price_SBUX)):
-  x_train.append(train_closing_price_SBUX[i-80:i,0])
-  y_train.append(train_closing_price_SBUX[i,0])
-
-
-# In[9]:
-
-
-# convert to numpy arrays
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-
-
-# In[10]:
-
-
-# Reshape the dataset
-x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
-
-
-# In[11]:
-
-
-x_train.shape
-
-
-# In[12]:
-
-
-# Building the LSTM model
-#x_train = tf.convert_to_tensor(x_train)
-#y_train = tf.convert_to_tensor(y_train)
-
-SBUX_model = Sequential()
-SBUX_model.add(LSTM(50, return_sequences=True, input_shape= (x_train.shape[1],1)))
-SBUX_model.add(LSTM(50, return_sequences=False))
-SBUX_model.add(Dense(25))
-SBUX_model.add(Dense(1))
-
-
-# In[13]:
-
-
-# Compile  the LSTM model
-SBUX_model.compile(optimizer='adam', loss='mean_squared_error')
-#'categorical_crossentropy'
-
-
-# In[14]:
-
-
-# training the AAPL_model
-SBUX_model.fit(x_train,y_train,epochs=1,batch_size=1)
-
-
-# In[15]:
-
-
-# Setting the testing Dataset
-# scaled testing dataset set from index 1743 to 2003
-SBUX_test_dataset= scaled_closing_price_SBUX_df[train_closing_price_SBUX_len - 80: , :]
-
-#setting the x and y test data
-x_test = []
-y_test = Closing_Price_array_SBUX_df[train_closing_price_SBUX_len, :]
-for i in range(80,len(SBUX_test_dataset)):
-    x_test.append(SBUX_test_dataset[i-80:i, 0])
-
-
-# In[16]:
-
-
-# Convert the test dataset to array
-x_test = np.array(x_test)
-print (x_test)
-
-
-# In[17]:
-
-
-#reshape the test dataset
-
-x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1],1))
-
-
-# In[18]:
-
-
-x_test.shape
-
-
-# In[19]:
-
-
-# getting the model predicted closing price value
-
-predictions = SBUX_model.predict(x_test)
-predictions = scale_SBUX.inverse_transform(predictions)
-
-
-# In[20]:
-
-
-# getting the root mean squared error 
-
-rmse = np.sqrt(np.mean(predictions - y_test) **2)
-rmse
-
-
-# 
-
-# In[21]:
-
-
-# Plot the Dataset
-train_data = Closing_Price_SBUX_df[:train_closing_price_SBUX_len]
-validation_data = Closing_Price_SBUX_df[train_closing_price_SBUX_len:]
-
-validation_data['Predictions'] = predictions
-plt.figure(figsize=(16,8))
-plt.title('SBUX Model', color='crimson')
-plt.xlabel('date', fontsize=18, color='crimson')
-plt.ylabel('closing price USD', color='crimson')
-plt.plot(train_data['Close'], color='darkcyan')
-plt.plot(validation_data['Close'], color='limegreen')
-plt.plot(validation_data['Predictions'], color = 'lightcoral')
-plt.legend(['Train', 'Validation', 'Predictions'])
-
-
-# In[ ]:
+from datetime import datetime
+import plotly.graph_objs as go
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+
+##### Layouts for Tab 1 #####
+# Dropdown list for single stock choice
+def get_tab1_info_box():
+	return html.Div([
+				html.Div([
+					html.Div(id='tab1-stock-price',
+						     style={'width':'30%','display':'inline-block',
+						            'font-size':'200%'}),
+					html.Div(id='tab1-stock-price-change'),
+					html.Div(id='tab1-stock-price-percentchange')
+					],style={'width':'30%','display': 'inline-block',
+			                 'vertical-align':'top'}),
+				html.Div(style={'width':'15%','display': 'inline-block'}),
+				html.Div(dcc.Dropdown(id='tab1-market-dropdown',
+					                  options=tab1_markets,
+					                  value=tab1_markets[0]['value'],
+				                      style={'text-align':'left'}),
+					     style={'width':'20%','display': 'inline-block',
+			            		'vertical-align':'top'}),
+				html.Div(style={'width':'5%','display': 'inline-block'}),
+				html.Div([
+					html.Div([
+						html.Div(dcc.Input(id='tab1-ticker-input',value='',
+						                   type='text'),
+						                   style={'display': 'inline-block'}),
+						html.Div(html.Button('Submit',id='tab1-submit'),
+							     style={'display': 'inline-block'})
+						]),
+					html.Div(id='tab1-error-message', style={'color':'red'}, children='Error Box')
+					],style={'width':'30%','display': 'inline-block'})
+			])
+
+##### Layout for Tab 2 #####
+# Dropdown list for index and stock growth difference
+def get_tab2_info_box():
+	return html.Div([
+			html.Div([dcc.Dropdown(id='tab2-index-choice',
+				                   options=index_choice,
+				                   value=index_choice[0]['value'],
+								   style={'text-align':'left'})],
+				style={'width':'30%','display': 'inline-block'}),
+			html.Div(style={'width':'20%','display': 'inline-block'}),
+			html.Div([dcc.Dropdown(id='tab2-stock-include',
+				                   options=[],
+				                   value=[],
+				                   multi=True,
+				                   style={'text-align':'left'})],
+				style={'width':'50%','display': 'inline-block'})
+		])
+
+##### Layout for both Tab 1 and Tab 2 #####
+# Layout for user to select time interval
+def get_interval_layout(tab):
+	id_name = tab+'-time-interval'
+	layout = html.Div(
+		dcc.RadioItems(id=id_name,
+                       options=[{'label':'1 Month', 'value':'1mo'},
+                                {'label':'3 Months', 'value':'3mo'},
+                                {'label':'6 Months', 'value':'6mo'},
+                                {'label':'YTD', 'value':'ytd'},
+                                {'label':'1 Year', 'value':'1y'},
+                                {'label':'3 Years', 'value':'3y'},
+                                {'label':'5 Years', 'value':'5y'}
+                       ],
+                       value='ytd'
+		), style={'text-align':'center'})
+	return layout
+
+# Layout for graph, include time interval and graph
+def get_graph_layout(tab):
+	layout = html.Div([
+		html.Br(),
+		get_interval_layout(tab),
+		dcc.Graph(id=tab+'-vis')
+		])
+	return layout
+
+# Layout for table for stats
+def get_table_layout(tab):
+	id_name = tab+'-table'
+	return html.Div([html.Br(),html.Br(),html.Table(id=id_name)])
+
+# Layout for stats and graph
+def get_stats_graph_layout(tab):
+	return html.Div([
+		html.Div(get_table_layout(tab),
+			     style={'width':'30%','display': 'inline-block',
+			            'vertical-align':'top'}),
+		html.Div(get_graph_layout(tab),
+			     style={'width':'70%','display': 'inline-block',
+			            'vertical-align':'top'})
+		])
+
+##### Generate Plots and Tables#####
+# Generate Line charts for stocks and index
+def getLinePlot(df, tab):
+	traces = []
+	for col in df.columns:
+		if col != 'Date':
+			traces.append({'x':df['Date'], 'y':df[col],'name':col,
+				           'mode':'lines'})
+	layout = {'xaxis':{'title':'Date'},
+	          'hovermode':False}
+	if tab == 1:
+		layout['yaxis'] = {'title':'Price ($)'}
+
+	elif tab == 2:
+		layout['yaxis'] = {'title':'% Change', 'tickformat':'.0%'}
+	return {'data':traces, 'layout':layout}
+
+# Generate candlestick
+def getCandlestick(df):
+	data = []
+	data.append(go.Candlestick(x=df['Date'], open=df['Open'],
+		                       high=df['High'], low=df['Low'],
+		                       close=df['Close']))
+	layout = {'xaxis':{'title':'Date','rangeslider':{'visible':False}},
+			  'yaxis':{'title':'Price ($)'},
+	          'hovermode':False}
+	return {'data':data, 'layout':layout}
+
+# Generate Table for Tab 1 - Stock Stats
+def getTab1Table(df, stock_info):
+	last_day = df.iloc[-1,1:6]
+	# Format the day range of price
+	low_day = last_day['Low']
+	high_day = last_day['High']
+	range_day = f'{low_day:,.2f}'+' - '+f'{high_day:,.2f}'
+	# Obtain and format 52 weeks range of price
+	low_52weeks = df['Low'].min()
+	high_52weeks = df['High'].max()
+	range_52weeks = f'{low_day:,.2f}'+' - '+f'{high_day:,.2f}'
+
+	# Format volume and average volume
+	vol = last_day['Volume']
+	vol = f'{vol:,.0f}'
+	avg_vol = stock_info['averageVolume10days']
+	avg_vol = f'{avg_vol:,.0f}'
+
+	#Obtain shares outstanding
+	shareOutstanding = stock_info['sharesOutstanding']
+
+	# Calculate market cap and format it
+	mktcap = last_day['Close']*shareOutstanding
+	mktcap = f'{mktcap:,.0f}'
+
+	# Format beta
+	beta = stock_info['beta']
+	beta = f'{beta:.2f}'
+
+	# Format PE and Forward PE, if no PE, PE not in the dictionary
+	if 'trailingPE' in stock_info:
+		pe = stock_info['trailingPE']
+		pe = f'{pe:.2f}'
+	else:
+		pe = 'N/A'
+	if 'forwardPE' in stock_info:
+		fpe = stock_info['forwardPE']
+		fpe = f'{fpe:.2f}'
+	else:
+		fpe = 'N/A'
+
+	# Format EPS
+	eps = stock_info['trailingEps']
+	eps = f'{eps:.2f}'
+
+	# Format Profit margin
+	margin = stock_info['profitMargins']
+	margin = f'{margin:.2f}'
+
+	# Prepare data for dividend rate
+	if stock_info['dividendRate'] is None or stock_info['dividendRate']=='':
+		dividend = 'N/A'
+	else:
+		dividend = stock_info['dividendRate']
+
+	# Prepare data for ex-dividend date
+	if stock_info['exDividendDate'] is not None:
+		ex_dividend_date = datetime.fromtimestamp(stock_info['exDividendDate'])
+		ex_dividend_date = ex_dividend_date.strftime('%m-%d-%Y')
+	else: 
+		ex_dividend_date = 'N/A'
+
+
+
+	return html.Table([
+		html.Tr([html.Td('Industry'), html.Td(),
+		         html.Td(stock_info['industry'])]),
+		html.Tr([html.Td('Previous Close'), html.Td(),
+			     html.Td(stock_info['previousClose'])]),
+		html.Tr([html.Td('Open'), html.Td(),
+			     html.Td(last_day['Open'])]),
+		html.Tr([html.Td('Day Range'), html.Td(),
+			     html.Td(range_day)]),
+		html.Tr([html.Td('52 Weeks Range'), html.Td(),
+			     html.Td(range_52weeks)]),
+		html.Tr([html.Td('Volume'), html.Td(),
+			     html.Td(vol)]),
+		html.Tr([html.Td('Average Volume'), html.Td(),
+			     html.Td(avg_vol)]),
+		html.Tr([html.Td('Market Capitalization'), html.Td(),
+			     html.Td(mktcap)]),
+		html.Tr([html.Td('Beta'), html.Td(),
+			     html.Td(beta)]),
+		html.Tr([html.Td('PE'), html.Td(),
+			     html.Td(pe)]),
+		html.Tr([html.Td('Forward PE'), html.Td(),
+			     html.Td(fpe)]),
+		html.Tr([html.Td('Earning Per Share (EPS)'), html.Td(),
+			     html.Td(eps)]),
+		html.Tr([html.Td('Profit Margin'), html.Td(),
+			     html.Td(margin)]),
+		html.Tr([html.Td('Dividend'), html.Td(),
+			     html.Td(dividend)]),
+		html.Tr([html.Td('Ex-Dividend Date'), html.Td(),
+			     html.Td(ex_dividend_date)]),
+		html.Tr([html.Td('Earning Per Share (EPS)'), html.Td(),
+			     html.Td(f'{shareOutstanding:,.0f}')])
+		])
+
+# Generate Table for Tab 2 - Index
+def getTab2Table(name, last_close, range_period, range_52weeks):
+	if last_close >= 0:
+		color = 'green'
+	else:
+		color = 'red'
+	last_close = last_close = f'{last_close:.2f}%'
+	return html.Table([
+		html.Tr(html.Td(html.B(name))),
+		html.Tr([html.Td('Close'),
+			    html.Td(last_close, style={'color':color})]),
+		html.Tr([html.Td('Period Range'),
+			     html.Td(f'{range_period[0]:,.2f}'+ \
+			     	     ' - '+f'{range_period[1]:,.2f}')]),
+		html.Tr([html.Td('52 Weeks Range'),
+			     html.Td(f'{range_52weeks[0]:,.2f}'+\
+			     	     ' - '+f'{range_52weeks[1]:,.2f}')])
+		])
+
+##### Global Variables #####
+# Dictionary for the dropdown list to select stock market
+tab1_markets = [{'label':'Hong Kong', 'value':'hk'},
+                {'label':'United States', 'value':'us'}]
+# Dictionary for the dropdown list to select index in Tab 2
+index_choice = [{'label':' Hong Kong: Heng Seng Index', 'value':'hsi'},
+                {'label':' United States: S&P 500', 'value':'sp500'}]
 
 
 
